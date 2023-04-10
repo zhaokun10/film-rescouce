3e008f405c32389eca97e96b45badd89621819c2
赵坤 <1733708465@qq.com>
2023-04-10 11:06:37 +0800

    项目初始

---
+++ src/main/java/com/sht/filmrescource/FilmRescouceApplication.java
@@ -0,0 +1,13 @@
+package com.sht.filmrescource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmRescouceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmRescouceApplication.class, args);
+    }
+
+}
